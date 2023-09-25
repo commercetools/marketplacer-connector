5ba3ee53b261de1a0abaf49b9e226eaddb00f7cb
praveen.kumar@commercetools.com <praveen.kumar@commercetools.com>
2023-09-25 21:55:46 +0200

    Test setup

---
+++ create-variants-function/src/test/java/com/commercetools/connect/marketplacer/ConnectorApplicationTest.java
@@ -0,0 +1,11 @@
+package com.commercetools.connect.marketplacer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConnectorApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
